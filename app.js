4e60f03193f8b7524bb69be4407e3eeb6e830d6f
Max Yankov <max.yankov@glassesoff.com>
2017-03-03 11:07:21 +0200

    Trying to fix redis URL...

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const Redis = require("redis");
 const token = "180445993:AAHghLnBrO-e5HgD-1X_J9V1XBQ_qwslpL4";
 
 const bot = new TelegramBot(token, {polling: true});
-console.log("Environment redis url: " + process.env.REDIS_URL);
-const REDIS_URL = process.env.REDIS_URL || "127.0.0.1:6379";
-const redisClient = Redis.createClient(REDIS_URL);
+const redisClient = Redis.createClient(process.env.REDIS_URL);
 
 redisClient.on("error", (err) => { console.error("Redis error: " + err); });
 
